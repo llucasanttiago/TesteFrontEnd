0e201cb02e6595151df5444e4e80f5b13f7fddea
Lucas Santiago <llucasanttiago@gmail.com>
2022-08-30 18:59:36 -0300

    Criação da home, login e cadastro

---
+++ src/views/CadastroView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="form">
+    <h1>Tela de cadastro de clientes</h1>
+
+    <form>
+    <h3>DADOS PESSOAIS</h3>
+      <label for="cpf">Insira seu CPF: </label>
+      <input type="text" id="cpf"><br><br>
+
+      <label for="nome">Nome: </label>
+      <input type="text" id="nome"><br><br>
+
+      <label for="rg">RG: </label>
+      <input type="text" id="rg"><br><br>
+
+      <label for="data-expedicao">Data de expedicao: </label>
+      <input type="date" id="data-expedicao"><br><br>
+
+      <label for="orgao-expedicao">Orgão de expedicao: </label>
+      <input type="text" id="orgao-expedicao"><br><br>
+
+      <label for="uf-expedicao">UF: </label>
+      <input type="text" id="uf"><br><br>
+
+      <label for="data-nascimento">Data de nascimento: </label>
+      <input type="date" id="data-nascimento"><br><br>
+
+      <label for="sexo">Sexo: </label>
+      <input type="text" id="sexo"><br><br>
+
+      <label for="estado-civil">Estado civil: </label>
+      <input type="text" id="estado-civil"><br><br>
+
+      <h3>ENDEREÇO</h3>
+      <label for="cep">CEP: </label>
+      <input type="text" id="cep"><br><br>
+
+      <label for="logradouro">Logradouro: </label>
+      <input type="text" id="logradouro"><br><br>
+
+      <label for="numero">Número: </label>
+      <input type="text" id="numero"><br><br>
+
+      <label for="complemento">Complemento: </label>
+      <input type="text" id="complemento"><br><br>
+
+      <label for="bairro">Bairro: </label>
+      <input type="text" id="bairro"><br><br>
+
+      <label for="cidade">Cidade: </label>
+      <input type="text" id="cidade"><br><br>
+
+      <label for="uf-endereco">UF: </label>
+      <input type="text" id="uf-endereco"><br><br>
+      <button>Enviar</button>
+    </form>
+
+  </div>
+</template>
+
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="tela-login">
+        <h1>LOGIN</h1>
+        <br>
+        <input type="text" placeholder="Usuário">
+        <br><br>
+        <input type="password" placeholder="Senha">
+        <br><br>
+        <input class="button" type="button" value="Entrar">
+        <br><br>
+        <a href="Cadastro.vue"><input class="button" type="button" value="Cadastrar"></a>
+    </section>
+
+    
+</template>
+
+<script>
+export default{
+    name:'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
